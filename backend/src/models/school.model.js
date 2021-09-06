ba851c3ee8818bfe25a2d12d537a02f5f37ab02b
Varga Jenő <jeno_varga@yahoo.com>
2021-09-06 13:21:21 +0200

    feladat-2

--- backend/src/models/school.model.js
+++ backend/src/models/school.model.js
@@ -1,3 +1,30 @@
 /**
  * Exercise: make School mongoose model
  */
+ const mongoose = require('mongoose');
+
+ const SchoolSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    },
+    classrooms: [
+        { type: mongoose.Schema.Types.ObjectId, ref: 'Classroom' }
+    ]
+ }, {
+     timestamps: true
+ });
+ 
+ module.exports = new mongoose.model('School', SchoolSchema, 'schools');
